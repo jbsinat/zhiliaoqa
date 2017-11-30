6346283392bc7045c000872e903603eade0a9116
jiang <jiangbiao0903@163.com>
2017-11-30 20:59:11 +0800

    临时存档

--- zhiliaoqa.py
+++ zhiliaoqa.py
@@ -1,13 +1,18 @@
-from flask import Flask
-import config
+from flask import Flask, render_template
+# import config
 
 app = Flask(__name__)
-app.config.from_object(config)
+# app.config.from_object(config)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/test/')
+def test():
+    return 'test page'
 
 
 if __name__ == '__main__':
